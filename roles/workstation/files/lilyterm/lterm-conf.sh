de6811c55435c1f8dd3fb97f0a5acebac7724225
sberts <sberts@sberts.com>
2021-12-09 04:00:15 +0000

    adding lilyterm background images

--- roles/workstation/files/lilyterm/lterm-conf.sh
+++ roles/workstation/files/lilyterm/lterm-conf.sh
@@ -4,9 +4,11 @@ PATH="/bin:usr/bin"
 
 i=0
 
+FOLDER=${1:-ubuntu}
+
 while [ $i -lt 42 ]; do
-  cat ~/.config/lilyterm/default.conf | \
-    sed "s#^background_image.*#background_image = /home/${1}/wallpaper/resized${i}.jpg#" \
-    > /home/${1}/.config/lilyterm/lilyterm${i}.conf
+  cat /home/${FOLDER}/.config/lilyterm/default.conf | \
+    sed "s#^background_image.*#background_image = /usr/local/share/wp/resized${i}.jpg#" \
+    > /home/${FOLDER}/.config/lilyterm/lilyterm${i}.conf
   i=$(($i+1))
 done
