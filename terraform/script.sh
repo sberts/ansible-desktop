51296f99f8efb08b71fc061d260f58ce66057c49
sberts <sberts@sberts.com>
2021-12-18 18:29:29 +0000

    updating script.sh to format and mount /dev/vdb

--- terraform/script.sh
+++ terraform/script.sh
@@ -3,4 +3,10 @@
 apt update && apt upgrade -y && apt install ansible -y && \
   ansible-pull -U https://github.com/sberts/ansible-desktop.git
 
+mkdir /data
+if ! mount /dev/vdb /data; then
+  mkfs.ext4 /dev/vdb
+  mount /dev/vdb /data
+fi
+
 echo date > /tmp/script.log
